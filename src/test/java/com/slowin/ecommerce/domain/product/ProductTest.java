58559b5bff09e942b2c522338be36c799d4ab2ca
wooobo <aogora2431@gmail.com>
2022-01-21 22:52:50 +0900

    test : Product 판매상태 변경 테스트 추가

--- src/test/java/com/slowin/ecommerce/domain/product/ProductTest.java
+++ src/test/java/com/slowin/ecommerce/domain/product/ProductTest.java
@@ -1,27 +1,43 @@
 package com.slowin.ecommerce.domain.product;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.slowin.ecommerce.domain.product.optionGroup.ProductOptionGroups;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
 
 public class ProductTest {
 
     @Test
-    void create() {
+    void changeOnSale_판매시작일_오늘보다_이후면_실패() {
+        // given
+        LocalDateTime saleStartAt = LocalDateTime.now().plusDays(1);
+        LocalDateTime saleEndAt = LocalDateTime.now().plusDays(2);
+        SalesPeriod salesPeriod = new SalesPeriod(saleStartAt, saleEndAt);
+        Product product = 상품생성(salesPeriod);
+
+        // when
+        ThrowableAssert.ThrowingCallable actual = product::changeOnSale;
+
+        // then
+        assertThatThrownBy(actual).isInstanceOf(RuntimeException.class);
+    }
+
+    private Product 상품생성(SalesPeriod salesPeriod) {
         // given
         String name = "청바지";
         Price productPrice = Price.of(1000L);
         String content = "상품 내용\n 내용 내용";
-        SalesPeriod salesPeriod = new SalesPeriod(LocalDateTime.now(),
-            LocalDateTime.now().plusDays(7));
+        Status status = Status.READY;
         String brandCode = "brx_01";
         String categoryCode = "ctx_01";
         String shippingPlaceCode = "sp_01";
         String returnPlaceCode = "rp_01";
         ProductOptionGroups productOptionGroups = new ProductOptionGroups(Collections.emptyList());
 
-        Product product = new Product(name, productPrice, content, productOptionGroups, salesPeriod,
+        return new Product(name, productPrice, content, status, productOptionGroups, salesPeriod,
             brandCode, categoryCode, shippingPlaceCode, returnPlaceCode);
     }
 }
