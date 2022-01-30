e892e6fbd6c481e129e34a1fb4bd10190fb0d8a2
wooobo <aogora2431@gmail.com>
2022-01-30 23:17:48 +0900

    feat : ShippingPolicy 도메인 추가

---
+++ src/main/java/com/slowin/ecommerce/domain/shippingpolicy/PlaceAddress.java
@@ -0,0 +1,50 @@
+package com.slowin.ecommerce.domain.shippingpolicy;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class PlaceAddress {
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address_detail")
+    private String addressDetail;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    protected PlaceAddress() {
+    }
+
+    public PlaceAddress(String zipCode, String address, String addressDetail, String phoneNumber) {
+        this.zipCode = zipCode;
+        this.address = address;
+        this.addressDetail = addressDetail;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PlaceAddress that = (PlaceAddress) o;
+        return Objects.equals(zipCode, that.zipCode) && Objects.equals(address, that.address)
+            && Objects.equals(addressDetail, that.addressDetail) && Objects.equals(phoneNumber,
+            that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/com/slowin/ecommerce/domain/shippingpolicy/ShippingPolicy.java
@@ -0,0 +1,52 @@
+package com.slowin.ecommerce.domain.shippingpolicy;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ShippingPolicy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "usable")
+    private boolean usable;
+
+    @Embedded
+    private PlaceAddress placeAddress;
+
+    protected ShippingPolicy() {
+    }
+
+    public ShippingPolicy(String name, boolean usable, PlaceAddress placeAddress) {
+        this.name = name;
+        this.usable = usable;
+        this.placeAddress = placeAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ShippingPolicy that = (ShippingPolicy) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
