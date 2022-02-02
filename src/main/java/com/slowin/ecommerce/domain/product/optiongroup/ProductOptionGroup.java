5ab60b29c9dde78a48a4dcf4ce53b26e1e89fe3a
wooobo <aogora2431@gmail.com>
2022-02-02 23:18:09 +0900

    feat : ProductOptionItem 추가

--- src/main/java/com/slowin/ecommerce/domain/product/optiongroup/ProductOptionGroup.java
+++ src/main/java/com/slowin/ecommerce/domain/product/optiongroup/ProductOptionGroup.java
@@ -1,10 +1,17 @@
 package com.slowin.ecommerce.domain.product.optiongroup;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ProductOptionGroup {
@@ -15,6 +22,10 @@ public class ProductOptionGroup {
 
     private Integer ordering;
     private String productOptionGroupName;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_option_group_id", foreignKey = @ForeignKey(name = "fk_product_option_group_product_option_item"), nullable = false)
+    private List<ProductOptionItem> productOptionItems = new ArrayList<>();
 
     protected ProductOptionGroup() {
     }
